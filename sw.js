e63bc2763b949172674418163fc975a10b071cb6
Marcin Dłubis <m.dlubis.ext@coman-software.com>
2024-06-14 20:37:04 +0200

    rest

---
+++ sw.js
@@ -0,0 +1,30 @@
+var cacheName = "podyplomowe";
+var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    console.log('installing sw');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                var x = cache.addAll(urlsToCache);
+                console.log('cache added');
+                return x;
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
